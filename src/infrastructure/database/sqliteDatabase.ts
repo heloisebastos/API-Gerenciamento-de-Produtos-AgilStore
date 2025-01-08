e27ec91cc10c05a5573ed4cdc193dd182d2a124e
heloisebastos <heloisebastos28@hotmail.com>
2025-01-08 15:58:31 -0400

    feat: adiciona o esquema do banco de dados para armazenar informações dos produtos

---
+++ src/infrastructure/database/sqliteDatabase.ts
@@ -0,0 +1,20 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err.message);
+    } else {
+        console.log('Conectado ao banco de dados SQLite.');
+        db.run(
+            `CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        category TEXT NOT NULL,
+        quantity INTEGER NOT NULL,
+        price REAL NOT NULL
+      )`
+        );
+    }
+});
+
+export default db;
