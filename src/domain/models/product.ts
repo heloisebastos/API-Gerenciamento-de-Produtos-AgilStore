97501debc27989cb67ec9087c72bf9941190bdb1
heloisebastos <heloisebastos28@hotmail.com>
2025-01-08 22:13:30 -0400

    chore: remove trechos de comentário no código

--- src/domain/models/product.ts
+++ src/domain/models/product.ts
@@ -8,29 +8,22 @@ export class Product {
     ) { }
 
     validate(): void {
-        // Validação para nome
         if (!this.name || typeof this.name !== 'string' || this.name.trim().length === 0) {
             throw new Error('O nome do produto é inválido.');
         }
 
-        // Validação para categoria
         if (!this.category || typeof this.category !== 'string' || this.category.trim().length === 0) {
             throw new Error('A categoria do produto é inválida.');
         }
 
-        // Validação para quantidade
         if (this.quantity == null || !Number.isInteger(this.quantity) || this.quantity < 0) {
             throw new Error('Erro ao inserir quantidade de produto');
         }
 
-        // Validação para preço
         if (this.price == null || typeof this.price !== 'number' || this.price < 0) {
             throw new Error('O preço do produto é inválido.');
         }
 
 
-        // if (!this.name || !this.category || this.quantity < 0 || this.price < 0) {
-        //     throw new Error('Dados inválidos para o produto.');
-        // }
     }
 }
